3767648236ec77a3129ee5ddaf80858f1a7f52b4
lapael1 <lapael6@gmail.com>
2024-07-17 13:24:05 +0900

    인터파크 웹사이트 클론코딩 티켓랭킹

--- js/main.js
+++ js/main.js
@@ -82,101 +82,77 @@ window.onload = function () {
     },
   });
 
- // 투어의 스와이퍼 기능적용
- const swTour = new Swiper(".sw-tour",{
-  navigation: {
-    nextEl: ".tour .sw-next",
-    prevEl: ".tour .sw-prev",
-  },
-  pagination: {
-    el: ".sw-tour-pg ",
-    clickable: true,
-  },
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  // 모바일버전
-  slidesPerView: 1,
-  grid: {
-    rows: 1,
-    fill: "row",
-  },
-  
-  breakpoints: {
+  // 투어의 스와이퍼 기능적용
+  const swTour = new Swiper(".sw-tour", {
+    navigation: {
+      nextEl: ".tour .sw-next",
+      prevEl: ".tour .sw-prev",
+    },
+    pagination: {
+      el: ".sw-tour-pg ",
+      clickable: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    // 모바일버전
+    slidesPerView: 1,
+    grid: {
+      rows: 1,
+      fill: "row",
+    },
+
+    breakpoints: {
+      480: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
+        1024: {
+          spaceBetween: 32,
+          slidesPerView: 3,
+          slidesPerGroup: 2,
+          grid: {
+            rows: 1,
+          },
+        },
+        1280: {
+          spaceBetween: 26,
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          grid: {
+            rows: 1,
+          },
+        },
+      },
+    },
+  });
+  // 티켓 스와이퍼 적용
+const swTicket = new Swiper(".sw-ticket" ,{
+  loop:true,
+  breakpoints : {
     480: {
       spaceBetween: 32,
-      slidesPerView: 3,
-      slidesPerGroup:2,
-      grid: {
-        rows: 1,
-      },
-    1024: {
-      spaceBetween: 32,
-      slidesPerView: 3,
-      slidesPerGroup: 2,
-      grid: {
-        rows: 1,
-      },
-    },
-    1280: {
-      spaceBetween: 26,
-      slidesPerView:3,
-      slidesPerGroup: 3,
-      grid: {
-        rows: 1,
-      },
-    },
-  },
-
-} })};
-
-// 티켓 스와이퍼 적용
-const swTicket = new Swiper(".sw-ticket", {
-  slidesPerView: 1,
-  grid: {
-    rows: 1,
-    fill: "row",
-  },
-  navigation: {
-    nextEl: ".ticket .sw-next",
-    prevEl: ".ticket .sw-prev",
-  },
-  pagination: {
-    el: ".sw-ticket-pg ",
-    clickable: true,
-  },
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  spaceBetween: 10,
-  breakpoints: {
-    900: {
-      spaceBetween: 32,
-      slidesPerView: 4,
-      grid: {
-        rows: 1,
-      },
-      slidesPerGroup: 3,
+      slidesPerView: 2,
+     
+     
     },
     1024: {
       spaceBetween: 32,
       slidesPerView: 3,
-      slidesPerGroup: 3,
-      grid: {
-        rows: 1,
-      },
+    
+   
     },
     1280: {
       spaceBetween: 26,
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-      grid: {
-        rows: 1,
-      },
+      slidesPerView:4,
+      
+    
     },
   },
-});
+})
+};
