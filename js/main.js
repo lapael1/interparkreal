7865f7b5e593ea5c1595af3eea8c79a07a69c1c1
lapael1 <lapael6@gmail.com>
2024-07-22 13:11:16 +0900

    인터파크 클론코딩 연습

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload = function () {
   // 상단의 마이페이지리스트 기능
   const mypageBt = document.querySelector(".mypage-bt");
   const mypageList = document.querySelector(".mypage-list");
-  mypageBt.addEventListener("mouseover", function () {
+  mypageBt.addEventListener("click", function () {
     mypageList.classList.toggle("showMypage");
   });
   // swiper ================
@@ -35,29 +35,29 @@ window.onload = function () {
 
   // 쇼핑의 스와이퍼 기능적용
   const swShopping = new Swiper(".sw-shopping", {
-    slidesPerView: 1,
-    grid: {
-      rows: 1,
-      fill: "row",
-    },
     navigation: {
       nextEl: ".shopping .sw-next",
       prevEl: ".shopping .sw-prev",
     },
-    pagination: {
-      el: ".sw-shopping-pg ",
-      clickable: true,
-    },
     loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+    slidesPerView: 1.1,
+    grid: {
+      rows: 1,
+      fill: "row",
     },
     spaceBetween: 10,
     breakpoints: {
-      900: {
+      480: {
         spaceBetween: 32,
-        slidesPerView: 4,
+        slidesPerView: 2,
+        grid: {
+          rows: 1,
+        },
+        slidesPerGroup: 3,
+      },
+      680: {
+        spaceBetween: 32,
+        slidesPerView: 3,
         grid: {
           rows: 1,
         },
@@ -81,58 +81,57 @@ window.onload = function () {
       },
     },
   });
-
   // 투어의 스와이퍼 기능적용
   const swTour = new Swiper(".sw-tour", {
-    navigation: {
-      nextEl: ".tour .sw-next",
-      prevEl: ".tour .sw-prev",
-    },
-    pagination: {
-      el: ".sw-tour-pg ",
-      clickable: true,
-    },
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
     // 모바일버전
-    slidesPerView: 1,
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    slidesPerGroup: 2,
     grid: {
       rows: 1,
       fill: "row",
     },
-
+    navigation: {
+      nextEl: ".tour .sw-next",
+      prevEl: ".tour .sw-prev",
+    },
+    loop: true,
     breakpoints: {
       480: {
+        spaceBetween: 32,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 2,
+        },
+      },
+      1024: {
         spaceBetween: 32,
         slidesPerView: 3,
         slidesPerGroup: 2,
         grid: {
           rows: 1,
         },
-        1024: {
-          spaceBetween: 32,
-          slidesPerView: 3,
-          slidesPerGroup: 2,
-          grid: {
-            rows: 1,
-          },
-        },
-        1280: {
-          spaceBetween: 26,
-          slidesPerView: 3,
-          slidesPerGroup: 3,
-          grid: {
-            rows: 1,
-          },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
         },
       },
     },
   });
-  // 티켓 스와이퍼 적용
+  // ticket스와퍼
   const swTicket = new Swiper(".sw-ticket", {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".ticket .sw-next",
+      prevEl: ".ticket .sw-prev",
+    },
+
     loop: true,
     breakpoints: {
       480: {
@@ -147,62 +146,120 @@ window.onload = function () {
         spaceBetween: 26,
         slidesPerView: 4,
       },
-      navigation: {
-        nextEl: ".ticket .sw-next",
-        prevEl: ".ticket .sw-prev",
+    },
+  });
+  // live스와퍼
+  const swLive = new Swiper(".sw-live", {
+    navigation: {
+      nextEl: ".live .sw-next",
+      prevEl: ".live .sw-prev",
+    },
+
+    loop: true,
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    breakpoints: {
+      330: {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
       },
-      pagination: {
-        el: ".sw-ticket-pg ",
-        clickable: true,
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
       },
-      loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+
+      675: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 27,
       },
     },
   });
-
-  // live스와퍼
-  const swLive = new Swiper(".sw-live", {
-    breakpoints: {
-      320: {
-        spaceBetween: 12,
-        slidesPerView: 2,
-      },
-      768: {
-        spaceBetween: 32,
-        slidesPerView: 3,
-      },
-      1280: {
-        spaceBetween: 26,
-        slidesPerView: 4,
-  }}});
-
-  // 북 스와이퍼 적용
-  const swBook = new Swiper(".sw-books", {
+  // books스와퍼
+  const bookx = new Swiper(".sw-books", {
     navigation: {
       nextEl: ".books .sw-next",
       prevEl: ".books .sw-prev",
     },
-    pagination: {
-      el: ".sw-books-pg ",
-      clickable: true,
+
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 19,
+    grid: {
+      rows: 5,
+      fill: "row",
     },
     breakpoints: {
-      320: {
-        spaceBetween: 12,
+      480: {
         slidesPerView: 2,
+        spaceBetween: 20,
+        grid: {
+          rows: 1,
+        },
       },
       768: {
-        spaceBetween: 32,
         slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        slidesPerGroup: 3,
+        spaceBetween: 30,
+        grid: {
+          rows: 1,
+        },
       },
       1280: {
-        spaceBetween: 26,
-        slidesPerView: 4,
-   
-        
-        
-  }}});
-};
+        slidesPerView: 5,
+        slidesPerGroup: 5,
+        spaceBetween: 27,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
+
+// 이벤트 스와이퍼
+const swEvent = new Swiper(".sw-event", {
+  navigation: {
+    nextEl: ".event .sw-next",
+    prevEl: ".event .sw-prev",
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      grid: {
+        rows: 1,
+      },
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      slidesPerGroup: 3,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+      },
+    },
+    1280: {
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+      spaceBetween: 12,
+      grid: {
+        rows: 1,
+      },
+    },
+  },
+})};
